0e9e68b2caaa4448d7b53cd5b4f11675e4a7fc0e
Lucy Wang <43558154+gujiguj@users.noreply.github.com>
2022-07-03 21:37:41 -0400

    add navbar from stream

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import '../stylesheets/App.css';
+
+// function should be same spelling as the component name
+function NavBar() {
+    // showLinks is a bool
+    // if showLinks is true, we hide the links
+    // and put them in a hamburger menu
+    // setShowLinks is used to set showLinks' value
+    const [hideLinks, setHideLinks] = useState(false);
+    return (
+        <nav className="navbar">
+            <div className="leftSide">
+                <div id={hideLinks ? "hidden" : ""}className="links">
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/contact">Contact</a>
+                </div>
+                <button onClick={() => setHideLinks(!hideLinks)}>Open</button>
+            </div>
+            <div className="rightSide">
+                <input type="text" placeholder="Hello!"></input>
+                <button className="search-button">Search</button>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
+
